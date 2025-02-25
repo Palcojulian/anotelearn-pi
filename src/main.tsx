383d132981acb7a8cf89cfd15f7bcaa6667866f0
palcojulian <palcojulian@outlook.com>
2025-02-24 23:27:13 -0500

    feat: :sparkles: Agregan rutas e importa layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import './index.css';
+import App from './App.tsx';
+import Layout from './layout/Layout.tsx';
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ <BrowserRouter>
+  <Layout>
+    <Routes>
+      <Route path='/' element={<App />} />
+    </Routes>
+  </Layout>
+ </BrowserRouter>
 )
