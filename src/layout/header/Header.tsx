4b0d8709fe76236a3753091565f82f9179dfc53d
David <julian.david.rengifo@correounivalle.edu.co>
2025-03-17 13:20:37 -0500

    se ajustaron los colores del header y footer ademas de cambiar al logo oficial de la empresa

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,5 +1,5 @@
 import './Header.css';
-import logo from './../../assets/icons/logo.jpg'
+import logo from './../../assets/icons/logo2.png'
 
 
 const Header = () => {
@@ -7,7 +7,7 @@ const Header = () => {
     <header>
       <nav className='nav'>
         <img src={logo} alt="" />
-        <h2>Anatolearn</h2>
+        {/* <h2>Anatolearn</h2> */}
       </nav>
     </header>
   )
